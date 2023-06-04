c0dbf6e7abae25e1387d2b5f48c4f07f448b56dc
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-06-04 14:48:49 +0900

    ぱっけーじもろもろ導入

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # riverpod2
 
-A new Flutter project.
+riverpod_generator によるコード生成を自動で行う
+
+`dart pub run build_runner watch`
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Home());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Home extends ConsumerWidget {
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
