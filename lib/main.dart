00609c728d31ddebf0db665660d4fc7fe3613480
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-06-04 16:15:27 +0900

    ジェネレート試し

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod2/firebase_options.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
+@riverpod
+String hello(HelloRef ref) => 'hello';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   /// Firebaseの初期化については今回の講座の範囲外なので、事前に済ませておいた。
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const Home());
+  runApp(const ProviderScope(child: Home()));
 }
 
 class Home extends ConsumerWidget {
@@ -16,8 +22,13 @@ class Home extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const MaterialApp(
-      home: Scaffold(),
+    final hello = ref.watch(helloProvider);
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(hello),
+        ),
+      ),
     );
   }
 }
---
+++ lib/main.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$helloHash() => r'8d478e92e3e96a8bf9e7ac98a0a55e386df38a68';
+
+/// See also [hello].
+@ProviderFor(hello)
+final helloProvider = AutoDisposeProvider<String>.internal(
+  hello,
+  name: r'helloProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$helloHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HelloRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
