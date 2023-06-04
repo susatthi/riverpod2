0ba4b2c0298d5421d89abfd4cc9067b6a71fd25b
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-06-04 15:05:29 +0900

    firebaseの初期化まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod2/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Firebaseの初期化については今回の講座の範囲外なので、事前に済ませておいた。
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Home());
 }
 
